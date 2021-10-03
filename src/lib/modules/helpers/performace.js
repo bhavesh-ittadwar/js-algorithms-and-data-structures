04365f55b4c15714097726b646d0051aa5bbb64b
Forkin' Dev <bhavesh.ittadwar26@gmail.com>
2021-10-04 00:07:58 +0530

    feat: add performance helper

---
+++ src/lib/modules/helpers/performace.js
@@ -0,0 +1,15 @@
+// getPerformance recieves a function and its parameters
+// and returns the time it took to execute the function in seconds
+
+const getPerformance = (callback, ...args) => {
+  let timeElapsed
+  let endTime
+  let startTime = performance.now()
+
+  callback(...args)
+  endTime = performance.now()
+  timeElapsed =  (endTime - startTime) / 1000
+  return `${timeElapsed} seconds`
+}
+
+export { getPerformance }
