d7f9da138cddd45f8baf09497c06dafd69665874
Bhavesh <bhavesh.ittadwar26@gmail.com>
2021-11-12 00:28:39 +0530

    fix: bubble sort loop

--- src/lib/modules/sort/bubble-sort/lib/sort.js
+++ src/lib/modules/sort/bubble-sort/lib/sort.js
@@ -6,7 +6,7 @@ const {swap} = require('../../../helpers/swap')
  * @returns sorted array arr
  */
 const bubbleSort = (arr) => {
-  for (let i = 0; i < arr.length; i++)
+  for (let i = arr.length - 1; i >= 0; i--)
     for(let j = 0; j < i; j++)
       if (arr[j] > arr[j+1]) swap(arr, j, j+1)
   return arr
